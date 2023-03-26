69406de5a960b260f4960d8d6aaaec1c26cc8ee8
Lucasabdala94 <83674006+Lucasabdala94@users.noreply.github.com>
2023-03-25 22:29:25 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SOE
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-rrv6nr)
---
+++ src/App.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <>
+      <div className="contenedor">
+        <div className="contenedorinput">
+          <label>ET 1</label>
+          <input className="Input" className="Input"></input>
+        </div>
+        <div className="contenedorinput">
+          <label>ET 2</label>
+          <input className="Input"></input>
+        </div>
+      </div>
+      <div className="contenedor">
+        <div className="contenedorinput">
+          <label>ARRANQUE</label>
+          <input className="Input"></input>
+        </div>
+        <div className="contenedorinput">
+          <label>ARRANQUE</label>
+          <input className="Input"></input>
+        </div>
+      </div>
+      <div className="contenedor">
+        <div className="contenedorinput">
+          <label>EMISION</label>
+          <input className="Input"></input>
+        </div>
+        <div className="contenedorinput">
+          <label>EMISION</label>
+          <input className="Input"></input>
+        </div>
+      </div>
+      <div className="contenedor">
+        <div className="contenedorinput">
+          <label>RECEPCION</label>
+          <input className="Input"></input>
+        </div>
+        <div className="contenedorinput">
+          <label>RECEPCION</label>
+          <input className="Input"></input>
+        </div>
+      </div>
+      <div className="contenedor">
+        <div className="contenedorinput">
+          <label>APERTURA</label>
+          <input className="Input"></input>
+        </div>
+        <div className="contenedorinput">
+          <label>APERTURA</label>
+          <input className="Input"></input>
+        </div>
+      </div>
+      <div className="contenedor">
+        <div className="contenedorinput">
+          <label>ARE</label>
+          <input className="Input"></input>
+        </div>
+        <div className="contenedorinput">
+          <label>ARE</label>
+          <input className="Input"></input>
+        </div>
+      </div>
+      <div className="contenedor">
+        <div className="contenedorinput">
+          <label>CERRADO</label>
+          <input className="Input"></input>
+        </div>
+        <div className="contenedorinput">
+          <label>CERRADO</label>
+          <input className="Input"></input>
+        </div>
+      </div>
+      <div className="contenedor">
+        <div className="contenedorinput">
+          <label>ARRANQUE</label>
+          <input className="Input"></input>
+        </div>
+        <div className="contenedorinput">
+          <label>ARRANQUE</label>
+          <input className="Input"></input>
+        </div>
+      </div>
+      <div className="contenedor">
+        <div className="contenedorinput">
+          <label>EMISION</label>
+          <input className="Input"></input>
+        </div>
+        <div className="contenedorinput">
+          <label>EMISION</label>
+          <input className="Input"></input>
+        </div>
+      </div>
+      <div className="contenedor">
+        <div className="contenedorinput">
+          <label>RECEPCION</label>
+          <input className="Input"></input>
+        </div>
+        <div className="contenedorinput">
+          <label>RECEPCION</label>
+          <input className="Input"></input>
+        </div>
+      </div>
+    </>
+  );
+}
